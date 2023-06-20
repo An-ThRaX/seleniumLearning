5ae513184099dd1095c3aa7f78ee93674e2735ab
An-ThRaX <ciprianionutz93@gmail.com>
2023-06-20 22:24:04 +0200

    some progress

---
+++ basicWeb/__init__.py
@@ -0,0 +1,3 @@
+# variables for chrome driver - home and work
+chrome_driver_home = "C:\\Users\\cipri\\PycharmProjects\\seleniumLearning\\drivers\\chromedriver.exe"
+chrome_driver_work = None
--- basicWeb/chrome_driver_windows.py
+++ basicWeb/chrome_driver_windows.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as Service
+from basicWeb import chrome_driver_home, chrome_driver_work
+
 
 class runChromeTests():
 
     def demo_test(self):
-        chrome_service = Service(executable_path="C:\\Users\\cipri\\PycharmProjects\\seleniumLearning\\drivers\\chromedriver.exe")
+        chrome_service = Service(executable_path=chrome_driver_home)
         # Instantiate browser
         driver = webdriver.Chrome(service=chrome_service)
         # Open the provided URL
